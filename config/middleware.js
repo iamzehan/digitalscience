1ca08511d1104d1c7fc73df0596a75500c51bcfa
Zehan <zehan.kareem@gmail.com>
2020-08-06 23:15:43 +0600

    Middlewares added

---
+++ config/middleware.js
@@ -0,0 +1,35 @@
+module.exports = {
+  //...
+  settings: {
+    cors: {
+      origin: 'http://localhost',
+    },
+  },
+};
+module.exports = ({ env }) => {
+  console.log('Initializing middleware.js...')
+  return {
+    timeout: 100,
+    load: {
+      before: ['responseTime', 'logger', 'cors', 'responses', 'gzip'],
+      order: [
+        "Define the middlewares' load order by putting their name in this array is the right order",
+      ],
+      after: ['parser', 'router'],
+    },
+    settings: {
+      public: {
+        path: './public',
+        maxAge: 60000,
+      },
+      favicon: {
+        path: 'favicon.ico',
+        maxAge: 86400000
+      },
+      security: {
+        enabled: true,
+        origin: 'HELLO'
+      }
+    }
+  };
+};
